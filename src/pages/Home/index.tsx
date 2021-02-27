064a2382949eb4fb990a39ef3e6be85c0665d739
Willian Del Freu <williankeyex@gmail.com>
2021-02-27 17:57:02 -0300

    chore: add search

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import EditIcon from '@material-ui/icons/Edit';
+import ClearIcon from '@material-ui/icons/Clear';
+import { useToasts } from 'react-toast-notifications';
+import { Table, Input } from 'semantic-ui-react';
 
 import Header from '../../components/Header';
-import Main from '../Main';
+
 import api from '../../services/api';
 
 interface Data {
@@ -19,24 +24,77 @@ interface Data {
 }
 
 function Home() {
-  const [data, setData] = useState<Data[]>([]);
-
-  const getData = () => {
-    return api.get('/usuarios').then(response => {
-      setData(response.data);
-    })
-  }
+  const [showData, setShowData] = useState<Data[]>([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const { addToast } = useToasts();
+  const history = useHistory();
 
   useEffect(() => {
-    getData()
-  }, []);
+    api.get(`usuarios?q=${searchTerm}`).then(response => {
+      setShowData(response.data);
+    });
+  }, [searchTerm]);
 
+  async function handleDeleteData(id: string) {
+    try {
+      await api.delete(`usuarios/${id}`).then(response => {
+        if (response.status === 200) {
+          addToast('Usuário Excluido Com Sucesso', { appearance: 'success' });
+        }
+      });
+    } catch (err) {
+      addToast('Erro ao deletar usuário', { appearance: 'error' });
+    }
+  }
+
+  function handleEditData(id: string) {
+    history.push(`/usuarios/${id}`);
+  }
+
+  const handleChange = (event: any) => {
+    setSearchTerm(event.target.value)
+  }
+  
   return (
-    <div id="page-landing">
-      <Header />
-      <Main data={data} getData={getData} />
-    </div>
-  );
+    <>
+    <Header />
+    <Table celled>
+      <Table.Header>
+        <Input 
+          icon='search' 
+          placeholder='Search...'
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </Table.Header>
+    </Table>
+    <Table celled>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Nome</Table.HeaderCell>
+          <Table.HeaderCell>CPF</Table.HeaderCell>
+          <Table.HeaderCell>E-mail</Table.HeaderCell>
+          <Table.HeaderCell>Cidade</Table.HeaderCell>
+          <Table.HeaderCell textAlign='center'>Editar</Table.HeaderCell>
+          <Table.HeaderCell textAlign='center'>Deletar</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      {showData.map((showData) => {
+        return (
+      <Table.Body key={showData.id}>
+        <Table.Row>
+          <Table.Cell>{[showData.name]}</Table.Cell>
+          <Table.Cell>{[showData.cpf]}</Table.Cell>
+          <Table.Cell>{[showData.email]}</Table.Cell>
+          <Table.Cell>{[showData.address.city]}</Table.Cell>
+          <Table.Cell textAlign='center'>{<EditIcon className="editIcon" onClick={() => handleEditData(showData.id)} type="button"/>}</Table.Cell>
+          <Table.Cell textAlign='center'>{<ClearIcon className="clearIcon" onClick={() => handleDeleteData(showData.id)} type='button'/>}</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+      )})}
+    </Table>
+    </>
+  )
 }
 
 export default Home;
